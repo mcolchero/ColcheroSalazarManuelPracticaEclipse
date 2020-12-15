12b19322d35eb0ff50a78c5d22bbe2b11a733bfc
mcolchero <alum.mcolcheros@iesalixar.org>
2020-12-15 19:38:09 +0100

    Commit modificación proyecto practica eclipse

---
+++ practica_eclipse_git/src/practica.java
@@ -0,0 +1,9 @@
+	public class practica
+	{
+	    // Tu programa comienza con una llamada a main().
+	    // Imprime "Hola Mundo" a la ventana de la terminal.
+	    public static void main(String args[])
+	    {
+	        System.out.println("Hola Mundo");
+	    }
+	}
